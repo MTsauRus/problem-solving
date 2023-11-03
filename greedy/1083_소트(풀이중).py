fb9f4d938d12c4bffe5bde2fd4679604afa8075d
MTsauRus <qksrurwnsql@naver.com>
2023-11-03 17:01:03 +0900

    231103

--- greedy/1083_소트(풀이중).py
+++ greedy/1083_소트(풀이중).py
@@ -1,40 +1,25 @@
 ### 소트 (G5)
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
 arr = list(map(int, input().split()))
-arr_rev = deque(sorted(arr, reverse=True))
 S = int(input())
-tmp = []
 
-cnt = 0
-
-while True:
-    if S > N:
-        if not arr_rev:
-            break
-        now_max = arr_rev.popleft()
-        idx = arr.index(now_max)
-        for i in range(idx, 0, -1):
-            if arr[i-1] < arr[i]:
-                arr[i], arr[i-1] = arr[i-1], arr[i]
-        S -= idx
-        cnt += 1
-    else:
-        now_max = arr_rev[-(S+1)]
-        idx = arr.index(now_max)
-        if S < idx-cnt:
-            for i in range(S):
-                for j in range(N-1):
-                    if arr[j] < arr[j+1]:
-                        arr[j], arr[j+1] = arr[j+1], arr[j]
-                        break
-        else:
-            for i in range(idx, cnt, -1):
-                if arr[i-1] < arr[i]:
-                    arr[i], arr[i-1] = arr[i-1], arr[i]
+change = S
+for i in range(N - 1):
+    if change == 0:
         break
+    max_now = arr[i]
+    max_idx = i
+    for j in range(i + 1, min(N, i + 1 + change)):
+        if arr[j] > max_now:
+            max_now = arr[j]
+            max_idx = j
+    
+    for k in range(max_idx, i, -1):
+        arr[k] = arr[k-1]
+    arr[i] = max_now
+    change -= (max_idx - i)
     
 print(*arr)
