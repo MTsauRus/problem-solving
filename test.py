208be78fb2a7c94716c71620ea1675874d878929
MTsauRus <qksrurwnsql@naver.com>
2024-03-22 16:08:11 +0900

    240322 커밋

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-import sys
-l=["Never gonna give you up",
-"Never gonna let you down",
-"Never gonna run around and desert you",
-"Never gonna make you cry",
-"Never gonna say goodbye",
-"Never gonna tell a lie and hurt you",
-"Never gonna stop"]
 a=int(input())
-ll=[]
-for i in range(a):
-    ll.append(input().strip())
-
-for next in ll:
-    if next not in l:
-        print("Yes")
-        exit(0)
-print("No")
+l=list(map(int,input().split()))
+test=[0]*10
+for i in l:
+    test[i]=1
+for j in range(10):
+    if test[j] == 1:
+        print(j)
