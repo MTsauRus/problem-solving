616a51e987a5adcf986603c4c556b727ff2fe093
MTsauRus <qksrurwnsql@naver.com>
2024-05-03 00:58:55 +0900

    240501 충돌방지

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-import math
-comb = math.comb(90, 2)
-p = comb * (0.007**2) * ((1-0.007)**88)
-print(str(100*p) + "%")
+a=int(input())
+b = list(input().strip())
+for i in range(a-1):
+    tmp = list(input().strip())
+    for j in range(len(b)):
+        if b[j] != tmp[j]:
+            b[j] = '?'
+
+for n in b:
+    print(n, end="")
