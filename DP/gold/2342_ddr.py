c2a138028127b3cce0d3e3dec83e753fc497bcbb
MTsauRus <qksrurwnsql@naver.com>
2025-11-11 16:10:19 +0900

    ddr 삭제 -> 추후 복구

--- DP/gold/2342_ddr.py
+++ DP/gold/2342_ddr.py
@@ -3,33 +3,33 @@
 import sys
 input = sys.stdin.readline
 
-arr = [0] + list(map(int, input().split()))
-arr.pop()
+nums = list(map(int, input().split()))
+nums.pop()
 
-# D[k][i][j]: 현재 발 i, j이고 k번 까지 고려했을 때의 최소점수
-D = [[[400000 for _ in range(5)] for _ in range(5)] for _ in range(len(arr)+10)]
+# D[k][i][j] : k번째까지 고려, 현재위치 i, j
+D = [[[400000 for _ in range(5)] for _ in range(5)] for _ in range(len(nums)+1)]
+F = [[1, 2, 2, 2, 2], 
+     [0, 1, 3, 4, 3],
+     [0, 3, 1, 3, 4],
+     [0, 4, 3, 1, 3],
+     [0, 3, 4, 3, 1]]
 
-# force[s][e] : s -> e 일 때 드는 힘
-force = [[0, 2, 2, 2, 2], 
-         [0, 1, 3, 4, 3], 
-         [0, 3, 1, 3 ,4], 
-         [0, 4, 3, 1, 3], 
-         [0, 3, 4, 3, 1]]
+D[0][0][0] = 0
+D[0][nums[0]][0] = 2
+D[0][0][nums[0]] = 2 # 처음 경우의 수는 왼발, 오른발을 각각 nums[0]에 두는 방법밖에 없음
 
-D[1][0][0] = 0
-D[1][arr[1]][0] = 2
-D[1][0][arr[1]] = 2
-for k in range(2, len(arr)):
+for k in range(len(nums)):
     for i in range(5):
         for j in range(5):
-            D[k][i][arr[k]] = min(D[k][i][arr[k]], D[k-1][i][j] + force[j][arr[k]])
-            D[k][arr[k]][j] = min(D[k][arr[k]][j], D[k-1][i][j] + force[i][arr[k]])
-
-ans = 4000000
+            # i가 움직이는 경우: 이전 칸 + 다음 움직였을 때의 힘의 합
+            D[k+1][nums[k]][j] = min(D[k+1][nums[k]][j], D[k][i][j] + F[i][nums[k]])
+            # j가 움직이는 경우
+            D[k+1][i][nums[k]] = min(D[k+1][i][nums[k]], D[k][i][j] + F[j][nums[k]]) 
+            
+ans = 400001
 for i in range(5):
     for j in range(5):
-        if ans > D[len(arr)-1][i][j]:
-            ans = D[len(arr)-1][i][j]
+        if ans > D[len(nums)][i][j]:
+            ans = D[len(nums)][i][j]
 
 print(ans)
-print(D)
