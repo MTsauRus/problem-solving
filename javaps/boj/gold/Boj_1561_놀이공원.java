5f7e9e81e62466a30b461f0fa000d6e26eda0937
MTsauRus <qksrurwnsql@naver.com>
2026-04-03 17:56:38 +0900

    260403

---
+++ javaps/boj/gold/Boj_1561_놀이공원.java
@@ -0,0 +1,22 @@
+package javaps.boj.gold;
+/*
+    1561. 놀이공원 (G1)
+    이분 탐색
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_1561_놀이공원 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+    
+        
+    
+    }
+}
