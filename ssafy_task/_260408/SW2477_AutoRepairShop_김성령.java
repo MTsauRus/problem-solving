a51b7a2659a9d73fdd931758d048166e407fd5a2
김성령-desktop <qksrurwnsql@naver.com>
2026-04-08 23:46:25 +0900

    260408

--- ssafy_task/_260408/SW2477_AutoRepairShop_김성령.java
+++ ssafy_task/_260408/SW2477_AutoRepairShop_김성령.java
@@ -3,76 +3,85 @@ package ssafy_task._260408;
 import java.io.*;
 import java.util.*;
 
-/*
-    1. 고객 도착 시간 정렬 -> 고객번호부여
-    2. 접수큐 통과시킴
-    3. 접수 통과 결과를 (시간, 접수큐번호, 고객번호)로 저장
-    4. 정비큐 입장 -> 
-        - 접수큐 번호가 요구와 동일하고, 정비큐가 동일하다면 ans++
-        - 
-*/
-
 public class SW2477_AutoRepairShop_김성령 {
-    static int recepCnt, repairCnt, custCnt, recepAnsNum, repairAnsNum;
-    static int[] recepTime, repairTime, custArriveTime;
-
-    static class Customer implements Comparable<Customer>{
-        int waitingTime, recepNum, repairNum;
-
-        public Customer(int waitingTime)  {
-            this.waitingTime = waitingTime;
-            this.recepNum = -1;
-            this.repairNum = -1;
-        }
-
-        @Override
-        public int compareTo(Customer o) {
-            if (this.waitingTime == o.waitingTime) return Integer.compare(this.recepNum, o.recepNum);
-            return Integer.compare(this.waitingTime, o.waitingTime);
-        }
-
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 1; t < T+1; t++) {
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N   = Integer.parseInt(st.nextToken()); 
+            int M   = Integer.parseInt(st.nextToken()); 
+            int K   = Integer.parseInt(st.nextToken()); 
+            int A   = Integer.parseInt(st.nextToken()) - 1; 
+            int B   = Integer.parseInt(st.nextToken()) - 1; 
+            int[] recepTime  = new int[N];
+            int[] repairTime = new int[M];
+            int[] arrival    = new int[K + 1]; 
+
             st = new StringTokenizer(br.readLine());
-            recepCnt = Integer.parseInt(st.nextToken());
-            repairCnt = Integer.parseInt(st.nextToken());
-            custCnt = Integer.parseInt(st.nextToken());
-            recepAnsNum = Integer.parseInt(st.nextToken());
-            repairAnsNum = Integer.parseInt(st.nextToken());
-            
+            for (int i = 0; i < N; i++) recepTime[i]  = Integer.parseInt(st.nextToken());
+
             st = new StringTokenizer(br.readLine());
-            recepTime = new int[recepCnt];
-            for (int i = 0; i < recepCnt; i++) {
-                recepTime[i] = Integer.parseInt(st.nextToken());
-            }
+            for (int i = 0; i < M; i++) repairTime[i] = Integer.parseInt(st.nextToken());
+
+            int[] tmp = new int[K];
             st = new StringTokenizer(br.readLine());
-            repairTime = new int[repairCnt];
-            for (int i = 0; i < repairCnt; i++) {
-                repairTime[i] = Integer.parseInt(st.nextToken());
-            }
-            st = new StringTokenizer(br.readLine());
-            custArriveTime = new int[custCnt];
-            for (int i = 0; i < custCnt; i++) {
-                custArriveTime[i] = Integer.parseInt(st.nextToken());
-            }
-            
-            PriorityQueue<Customer> custPq = new PriorityQueue<>();
-            for (int i = 0; i < custCnt; i++) {
-                custPq.offer(new Customer(custArriveTime[i]));
+            for (int i = 0; i < K; i++) tmp[i] = Integer.parseInt(st.nextToken());
+            Arrays.sort(tmp);
+            for (int i = 0; i < K; i++) arrival[i + 1] = tmp[i];
+
+            int[] recepFree        = new int[N];
+            int[] custRecepBooth   = new int[K + 1]; 
+            int[] custRecepFinish  = new int[K + 1]; 
+
+            for (int k = 1; k <= K; k++) {
+                int best = 0;
+                for (int i = 1; i < N; i++) {
+                    if (recepFree[i] < recepFree[best]) best = i;
+                }
+                int start = Math.max(arrival[k], recepFree[best]);
+                custRecepBooth[k]  = best;
+                custRecepFinish[k] = start + recepTime[best];
+                recepFree[best]    = custRecepFinish[k];
             }
 
-            while (!custPq.isEmpty()) {
-                Customer now = custPq.poll();
+            Integer[] order = new Integer[K];
+            for (int i = 0; i < K; i++) order[i] = i + 1;
+            Arrays.sort(order, (a, b) -> {
+                if (custRecepFinish[a] != custRecepFinish[b])
+                    return custRecepFinish[a] - custRecepFinish[b];
+                return custRecepBooth[a] - custRecepBooth[b];
+            });
 
-                // 
+            int[] repairFree      = new int[M];
+            int[] custRepairBooth = new int[K + 1]; 
+
+            for (int k : order) {
+                int best = 0;
+                for (int i = 1; i < M; i++) {
+                    if (repairFree[i] < repairFree[best]) best = i;
+                }
+                int start = Math.max(custRecepFinish[k], repairFree[best]);
+                custRepairBooth[k] = best;
+                repairFree[best]   = start + repairTime[best];
             }
+
+            int ans = 0;
+            boolean found = false;
+            for (int k = 1; k <= K; k++) {
+                if (custRecepBooth[k] == A && custRepairBooth[k] == B) {
+                    ans += k;
+                    found = true;
+                }
+            }
+
+            sb.append('#').append(t).append(' ').append(found ? ans : -1).append('\n');
         }
+
+        System.out.print(sb);
     }
 }
